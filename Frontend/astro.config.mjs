6e23ab8dc1da64024ff8d7288ab99c7d1082a7b5
Alejandro Garcia <israalegar789@gmail.com>
2026-01-28 01:14:49 -0600

    Vercel 3

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  adapter: vercel({
-    edgeMiddleware: false,
-  }),
   output: 'server',
+  adapter: vercel(),
 });
